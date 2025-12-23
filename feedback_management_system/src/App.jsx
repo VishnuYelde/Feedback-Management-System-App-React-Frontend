138706bd2ae0441db490ddc64ac0662a69a87fa9
Vishnu Yelde <vishnuyelde@gmail.com>
2025-12-24 01:01:38 +0530

    Initial frontend setup with React, Vite, Tailwind v3

---
+++ feedback_management_system/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-red-500">
+      <h1 className="text-5xl font-extrabold text-green-600">
+        Tailwind v3 WORKING
+      </h1>
+    </div>
+  )
+}
